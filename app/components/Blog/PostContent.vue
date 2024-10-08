aea3a729940d96829d80d322b499ecbd2fae29a9
Renato Lacerda <renatolacerda@pecege.com>
2024-10-08 13:08:38 -0300

    chore: styles and grid for blog content

--- app/components/Blog/PostContent.vue
+++ app/components/Blog/PostContent.vue
@@ -37,27 +37,86 @@ h2 {
   display: grid;
   grid-template-columns:
     [grid-start] 1fr
+    [breakout-start] 0
     [content-start] minmax(min(100%, 70ch), 1fr)
-    [content-end] 1fr
+    [content-end] 4vw
+    [breakout-end] 1fr
     [grid-end];
   margin-top: var(--space-l);
 
-  pre {
-    background-color: var(--bg-secondary);
-    margin-top: var(--space-s);
-    padding: 0.5rem 1rem;
-    border-radius: 0.4rem;
-    overflow-x: auto;
+  * {
+    grid-column: content;
   }
 
-  * {
-    grid-column: grid;
+  .breakout {
+    grid-column: breakout;
+  }
+
+  h2 {
+    font-size: var(--step-2);
+    max-width: 45ch;
+  }
+
+  h2,
+  h3 {
+    margin-top: var(--space-l);
   }
 
   p {
     margin-top: var(--space-xs);
-    line-height: 1.6;
-    grid-column: content;
+    line-height: 1.7;
+  }
+
+  pre {
+    grid-column: breakout;
+    background-color: var(--bg-secondary);
+    margin-block: var(--space-s);
+    padding: 0.5rem 1rem;
+    border-radius: 0.4rem;
+    overflow-x: auto;
+    font-size: var(--step--1);
+  }
+
+  :not(pre) > code {
+    display: inline-block;
+    padding-inline: 0.2rem;
+    background-color: var(--bg-secondary);
+    font-size: inherit;
+  }
+
+  :where(li + li) {
+    margin-top: 0.2em;
+  }
+
+  blockquote {
+    grid-column: breakout;
+
+    p {
+      font-size: var(--step-2);
+      color: var(--fg-details);
+      font-family: Optima;
+      font-style: italic;
+      line-height: 1.4;
+
+      &::before {
+        content: "“";
+      }
+      &::after {
+        content: "”";
+      }
+    }
+  }
+  sup a {
+    margin-inline: 0.1em;
+  }
+
+  .footnotes ol {
+    margin-top: 1.4em;
+  }
+
+  .stackblitz {
+    grid-column: grid;
+    margin-top: var(--space-l);
   }
 }
 </style>
