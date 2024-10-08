84c354b455342ab2c072c26a0a9eaf285c0c7a2b
Renato Lacerda <renato.ac.lacerda@gmail.com>
2024-10-08 20:14:05 -0300

    chore: re-adding p styles but for default layout only

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -10,6 +10,14 @@ main {
   margin-top: var(--space-xl-2xl);
 }
 
+:deep(p, li) {
+  max-width: 80ch;
+}
+
+:deep(p + p) {
+  margin-top: var(--space-s);
+}
+
 main > * {
   width: var(--content-size);
   margin-inline: auto;
