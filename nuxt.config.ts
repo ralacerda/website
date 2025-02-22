fa932ec6d98a19eff03c71b309c43f5468b56685
Renato Lacerda <renato.ac.lacerda@gmail.com>
2025-02-22 10:49:56 -0300

    fix: javascript code blocks missing syntax highglight

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,6 +34,7 @@ export default defineNuxtConfig({
     build: {
       markdown: {
         highlight: {
+          langs: ["javascript", "typescript", "css", "html", "vue"],
           theme: {
             default: "github-dark",
             dark: "github-dark",
