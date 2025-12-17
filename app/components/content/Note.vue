cacde1ae7337fd43db43c43735e56e3c33b0f4aa
Renato Lacerda <renato.ac.lacerda@gmail.com>
2025-12-17 14:37:29 -0300

    chore: new "note" component

---
+++ app/components/content/Note.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="note breakout">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.note {
+  padding: var(--space-s);
+  background-color: var(--bg-secondary);
+  margin-top: var(--space-l);
+  border-radius: 4px;
+
+  :deep(p) {
+    font-size: var(--step-0);
+    margin-top: var(--space-xs);
+
+    &:first-of-type {
+      margin-top: 0;
+    }
+  }
+}
+</style>
